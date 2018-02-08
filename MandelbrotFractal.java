41f243e5df79d14c924849bfba6983122ddd38b7
Michael Bisgaard Olesen <mbo@uber.com>
2018-02-08 23:23:48 +0100

    Minor optimization

--- MandelbrotFractal.java
+++ MandelbrotFractal.java
@@ -7,18 +7,17 @@ public class MandelbrotFractal implements Fractal {
     
     @Override
     public int getValue(double x, double y) {
-        double z_re = 0, z_im = 0;
-        double c_re = x, c_im = y;
-        
-        int iterations = 0;
+        double z_re = x;
+        double z_im = y;
         
         double z_re2 = z_re*z_re;
         double z_im2 = z_im*z_im;
         
+        int iterations = 1;
         while (needsMoreIterations(iterations) && isInCircle(z_re2, z_im2)) {
             double tmp = 2*z_re*z_im;
-            z_re = (z_re2 - z_im2) + c_re;
-            z_im = tmp + c_im;
+            z_re = (z_re2 - z_im2) + x;
+            z_im = tmp + y;
             
             // Update squared components.
             z_re2 = z_re*z_re;
@@ -30,11 +29,11 @@ public class MandelbrotFractal implements Fractal {
         return iterations;
     }
     
-    private boolean needsMoreIterations(int iterations) {
+    private static boolean needsMoreIterations(int iterations) {
         return iterations < Settings.MAX_ITERATIONS;
     }
     
-    private boolean isInCircle(double z_re2, double z_im2) {
+    private static boolean isInCircle(double z_re2, double z_im2) {
         return z_re2 + z_im2 <= Settings.BOUND_RADIUS_SQUARED;
     }
 }
